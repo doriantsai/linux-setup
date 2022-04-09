67d44844dccf80e22c87f91fcebdba8d03f7d8f3
Dorian Tsai <dorian.tsai@gmail.com>
2022-04-09 21:40:23 +1000

    art/media/graphics, google chrome, vscode installs

--- refresh_ubuntu_install_scripts.sh
+++ refresh_ubuntu_install_scripts.sh
@@ -18,6 +18,18 @@ sudo add-apt-repository restricted
 sudo apt install build-essential vim-gtk terminator htop openssh-server git gcc make cmake pkg-config zip unzip g++ curl dkms wget xclip synaptic gparted exfat-fuse exfat-utils guvcview net-tools ffmpeg ubuntu-restricted-extras chrome-gnome-shell
 sudo apt install openjdk-11-jdk libffi-dev
 
+# vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+# add pathogen/infect line to .vimrc
+echo -e "execute pathogen#infect()" >> ~/.vimrc
+echo -e "syntax on" >> ~/.vimrc
+echo -e "filetype plugin indent on" >> ~/.vimrc
+# install vim-sensible
+cd ~/.vim/bundle && \
+git clone https://github.com/tpope/vim-sensible.git
+# TODO: for vim: YouCompleteMe, rainbow
+
 # mamba-forge (better/smaller/faster than conda)
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
 bash Mambaforge-$(uname)-$(uname -m).sh
@@ -55,5 +67,45 @@ code --install-extension formulahendry.code-runner
 
 # android debug bridge - for communicating with emulator/light field camera (Lytro)
 sudo apt install adb
+
+###############################################################################################
+
+# google chrome
+sudo apt install libxss1 libappindicator1 libindicator7 # for chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome*.deb
+
+# art/media/graphics
+sudo apt install pulseaudio imagemagick
+sudo snap install vlc
+# install gimp
+sudo apt install flatpak
+sudo apt install gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# restart computer
+flatpak install https://flathub.org/repo/appstream/org.gimp.GIMP.flatpakref
+# flatpak run org.gimp.GIMP//stable
+flatpack update
+
+# install youtube-dl
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
+
+# inkscape
+sudo add-apt-repository ppa:inkscape.dev/stable
+sudo apt update
+sudo apt install inkscape
+
+# audacity
+sudo add-apt-repository ppa:ubuntuhandbook1/audacity
+sudo apt-get update
+sudo apt-get install audacity
+
+# # blender
+# sudo add-apt-repository ppa:thomas-schiex/blender &&
+# sudo apt-get update &&
+# sudo apt-get install blender
+
+
 
 
